3666bb2a1336fac70d75104af7a2ce96ae86d931
mconerv <60027611+mconerv@users.noreply.github.com>
2026-06-22 01:00:06 +0500

    Add files via upload

---
+++ functions/menus.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Menus
+---------------------------------------------------------------------------------------------------- */
+
+// Register Menus
+register_nav_menus( array(
+	'primary' => __( 'Primary Menu', 'fotonerv.art' ),
+	'secondary' => __( 'Secondary Menu', 'fotonerv.art' ),
+) );
